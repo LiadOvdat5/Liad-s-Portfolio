3137f6848d6a12e6b24163387e438d9c689561b7
LiadOvdat5 <LiadOvdat5@gmail.com>
2024-02-18 19:59:04 +0200

    Configure React app for deployment to GitHub Pages

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -10,7 +10,7 @@ const Main = () => {
         alt="main" />
         <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
           <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
-            <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I&aps;m Liad Ovdat</h1>
+            <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I&apos;m Liad Ovdat</h1>
             <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800"> I&apos;m 
 
             <TypeAnimation
